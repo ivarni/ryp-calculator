d2356438927d2ca3af4692588abc61196f3a5c01
Ivar Nilsen <post@ivarnilsen.com>
2016-10-16 09:50:36 +0000

    Dead code elimination squad

--- src/reducers/days.js
+++ src/reducers/days.js
@@ -9,8 +9,6 @@ for (let i = 0, l = 18; i < l; i++) {
     days[i] = i + 1;
 }
 
-let customCounter = 1;
-
 export const defaultDays = Immutable.List.of(...days.map((day) => {
     const formula = formulas[day - 1];
     return defaultExercises.map(exercise => exercise.merge({
@@ -19,34 +17,6 @@ export const defaultDays = Immutable.List.of(...days.map((day) => {
         sets: formula.sets[exercise.name],
     }));
 }));
-
-const updateExerciseValue = (state, action) =>
-    state.map((day, i) => {
-        const formula = formulas[i];
-        return day.map((exercise) => {
-            if (exercise.name === action.field) {
-                return exercise.merge({
-                    title: formula.title,
-                    value: (action.value * formula.multiplier).toFixed(1),
-                    sets: formula.sets[exercise.name],
-                });
-            }
-            return exercise;
-        });
-    });
-
-const updateExerciseLabel = (state, action) =>
-    state.map(day =>
-        day.map((exercise) => {
-            if (exercise.name === action.field) {
-                return exercise.merge({
-                    label: action.value,
-                    notes: action.notes,
-                });
-            }
-            return exercise;
-        })
-    );
 
 const exerciseFinished = (state, action) =>
     state.map((day, index) => {
@@ -61,22 +31,6 @@ const exerciseFinished = (state, action) =>
         return day;
     });
 
-const addExercise = (state, { label, value, notes }) => {
-    const name = `custom_${customCounter++}`;
-
-    return state.map((day, idx) => {
-        const formula = formulas[idx];
-
-        return day.concat({
-            name,
-            notes,
-            label,
-            value: (value * formula.multiplier).toFixed(1),
-            finished: false,
-        });
-    });
-};
-
 const updateDays = (state, action) =>
     state.map((day, dayIdx) => {
         const formula = formulas[dayIdx];
@@ -90,14 +44,8 @@ const updateDays = (state, action) =>
 
 export default (state = defaultDays, action) => {
     switch (action.type) {
-        case actions.FIELD_CHANGE:
-            return updateExerciseValue(state, action);
-        case actions.LABEL_CHANGE:
-            return updateExerciseLabel(state, action);
         case actions.EXERCISE_FINISHED:
             return exerciseFinished(state, action);
-        case actions.ADD_EXERCISE:
-            return addExercise(state, action);
         case actions.EXERCISES_UPDATED:
             return updateDays(state, action);
         default:
